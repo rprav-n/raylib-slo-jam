8bfa2f3738c2a47a04b75857a79f7f0c6678b4bc
Praveen <rpraveen.engineer@gmail.com>
2023-12-13 22:17:28 +0530

    Added player shoot bullet

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <cstdio>
+#include <vector>
 #include "raylib.h"
 #include "raymath.h"
 
@@ -7,101 +8,36 @@ using namespace std;
 const int WINDOW_WIDTH = 512;
 const int WINDOW_HEIGHT = 512;
 const float SCALE = 4.f;
-
-class Player
-{
-public:
-    Texture2D texture = LoadTexture("./assets/graphics/ships/green_ship.png");
-    int frame = 1;
-    int speed = 100;
-    float width = 0.f;
-    float height = 0.f;
-
-    float scaledWidth = 0.f;
-    float scaledHeight = 0.f;
-
-    Rectangle source = Rectangle{texture.width / 3.f, 0.f, texture.width / 3.f, (float)texture.height};
-    Vector2 position = {100.f, 100.f};
-    Rectangle dest = Rectangle{position.x, position.y, source.width *SCALE, (float)source.height *SCALE};
-    Vector2 origin = {};
-
-    Player(Vector2 pos)
-    {
-        position = pos;
-        width = texture.width / 3.f;
-        height = texture.height;
-
-        scaledWidth = width * SCALE;
-        scaledHeight = height * SCALE;
-
-        // origin = {width * SCALE / 2.f, height * SCALE / 2.f};
-    }
-
-    void Update(double dt)
-    {
-        Vector2 direction = {};
-        if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
-            direction.x = -1;
-        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
-            direction.x = 1;
-        if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W))
-            direction.y = -1;
-        if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S))
-            direction.y = 1;
-
-        direction = Vector2Normalize(direction);
-
-        if (direction.x < 0)
-            frame = 0;
-        if (direction.x > 0)
-            frame = 2;
-        if (direction.x == 0)
-            frame = 1;
-
-        source.x = width * frame;
-
-        position.x += direction.x * speed * dt;
-        position.y += direction.y * speed * dt;
-
-        CheckCollisionBounds();
-
-        dest.x = position.x;
-        dest.y = position.y;
-    }
-
-    void CheckCollisionBounds()
-    {
-        if (position.x <= 0)
-        {
-            position.x = 0;
-        }
-        if (position.x + scaledWidth >= WINDOW_WIDTH)
-        {
-            position.x = WINDOW_WIDTH - scaledWidth;
-        }
-    }
-
-    void Draw()
-    {
-        DrawTexturePro(texture, source, dest, origin, 0.f, WHITE);
-    }
-};
+const float BULLET_OFFSET_BOUND = 50.f;
 
 class Bullet
 {
 public:
-    Texture2D texture = LoadTexture("./assets/graphics/bullets/red_bullet.png");
-    Rectangle rect = Rectangle{0.f, 0.f, (float)texture.width, (float)texture.height};
+    Texture2D texture;
+    Rectangle rect = {};
     Vector2 position = {0.f, 0.f};
-    Rectangle dest = Rectangle{0.f, 0.f, texture.width *SCALE, texture.height *SCALE};
-    Vector2 origin = {texture.width * SCALE / 2.f, texture.height *SCALE / 2.f};
-    int speed = 100;
+    Rectangle dest = {};
+    Vector2 origin = {};
+    int speed = 400;
     float direction = 0.f;
 
-    Bullet(Vector2 pos, float dir)
+    Bullet(Texture txt, Vector2 pos, float dir)
     {
+        texture = txt;
+        rect = Rectangle{0.f, 0.f, (float)texture.width, (float)texture.height};
+        dest = Rectangle{0.f, 0.f, texture.width * SCALE, texture.height * SCALE};
+        origin = {texture.width * SCALE / 2.f, texture.height * SCALE / 2.f};
         position = pos;
         direction = dir;
+    }
+
+    bool IsOutOfBounds()
+    {
+        if (position.y < 0 || position.y > WINDOW_HEIGHT + BULLET_OFFSET_BOUND || position.x < -BULLET_OFFSET_BOUND || position.x > WINDOW_WIDTH + BULLET_OFFSET_BOUND)
+        {
+            return true;
+        }
+        return false;
     }
 
     void Update(float dt)
@@ -119,6 +55,126 @@ public:
     }
 };
 
+class Player
+{
+public:
+    Texture2D texture = LoadTexture("./assets/graphics/ships/green_ship.png");
+    Texture2D red_bullet = LoadTexture("./assets/graphics/bullets/red_bullet.png");
+    int frame = 1;
+    int speed = 250;
+    float width = 0.f;
+    float height = 0.f;
+
+    float scaledWidth = 0.f;
+    float scaledHeight = 0.f;
+
+    Rectangle source = Rectangle{texture.width / 3.f, 0.f, texture.width / 3.f, (float)texture.height};
+    Vector2 position = {100.f, 100.f};
+    Rectangle dest = Rectangle{position.x, position.y, source.width *SCALE, (float)source.height *SCALE};
+    Vector2 origin = {};
+    Vector2 centerOrigin = {};
+    vector<Bullet> bullets;
+
+    Player(Vector2 pos)
+    {
+        position = pos;
+        width = texture.width / 3.f;
+        height = texture.height;
+
+        scaledWidth = width * SCALE;
+        scaledHeight = height * SCALE;
+    }
+
+    void Update(double dt)
+    {
+        Vector2 direction = {};
+        if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
+            direction.x = -1;
+        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
+            direction.x = 1;
+        if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W))
+            direction.y = -1;
+        if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S))
+            direction.y = 1;
+
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            ShootBullet();
+        }
+
+        direction = Vector2Normalize(direction);
+
+        if (direction.x < 0)
+            frame = 0;
+        if (direction.x > 0)
+            frame = 2;
+        if (direction.x == 0)
+            frame = 1;
+        source.x = width * frame;
+
+        position.x += direction.x * speed * dt;
+        position.y += direction.y * speed * dt;
+
+        // position = Vector2MoveTowards(position, Vector2{position.x + direction.x * speed * (float)dt, position.y + direction.y * speed * (float)dt}, speed * dt);
+
+        for (int i = 0; i < bullets.size(); i++)
+        {
+            bullets[i].Update(dt);
+        }
+
+        // Remove bullet if it goes beyound the screen
+        for (int i = 0; i < bullets.size();)
+        {
+            Bullet b = bullets[i];
+            if (b.IsOutOfBounds())
+            {
+                bullets.erase(bullets.begin() + i);
+            }
+            else
+            {
+                ++i;
+            }
+        }
+
+        // printf("bullets.size() %lu\n", bullets.size());
+
+        CheckCollisionBounds();
+
+        dest.x = position.x;
+        dest.y = position.y;
+
+        centerOrigin.x = position.x + (scaledWidth / 2.f);
+        centerOrigin.y = position.y + (scaledHeight / 2.f);
+    }
+
+    void CheckCollisionBounds()
+    {
+        if (position.x <= 0)
+        {
+            position.x = 0;
+        }
+        if (position.x + scaledWidth >= WINDOW_WIDTH)
+        {
+            position.x = WINDOW_WIDTH - scaledWidth;
+        }
+    }
+
+    void Draw()
+    {
+        for (int i = 0; i < bullets.size(); i++)
+        {
+            bullets[i].Draw();
+        }
+        DrawTexturePro(texture, source, dest, origin, 0.f, WHITE);
+    }
+
+    void ShootBullet()
+    {
+        Bullet bullet = Bullet(red_bullet, centerOrigin, -1.f);
+        bullets.push_back(bullet);
+    }
+};
+
 int main()
 {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Edge Of Void");
@@ -128,7 +184,7 @@ int main()
     float bgY = 0.f;
 
     Player player = Player(Vector2{WINDOW_WIDTH / 2.f, WINDOW_HEIGHT / 2.f});
-    Bullet bullet = Bullet(Vector2{WINDOW_WIDTH / 2, 500.f}, -1.f);
+    // Bullet bullet = Bullet(Vector2{WINDOW_WIDTH / 2, 500.f}, -1.f);
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -151,9 +207,6 @@ int main()
 
         player.Update(dt);
         player.Draw();
-
-        bullet.Update(dt);
-        bullet.Draw();
 
         DrawFPS(10, 10);
 
